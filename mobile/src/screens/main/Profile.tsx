c5a3b7ce2a935a660e57fa3d41fbd8eeb84cf332
elliottgorrell <elliottgorrell1@gmail.com>
2024-11-01 10:22:38 +0100

    Fix layout on profile screen

--- mobile/src/screens/main/Profile.tsx
+++ mobile/src/screens/main/Profile.tsx
@@ -19,9 +19,9 @@ export default function Profile(): React.JSX.Element {
 
       <View style={tw`flex-2 bg-white rounded-3xl w-4/5`}>
         <View
-          style={tw`bg-neutral-500 relative -top-4 w-1/4 py-2 px-5 rounded-3xl self-center`}
+          style={tw`bg-neutral-500 relative -top-4 py-2 px-5 rounded-3xl self-center`}
         >
-          <Text style={tw`text-white  text-center`}>Profile</Text>
+          <Text style={tw`text-white  text-center text-base`}>Profile</Text>
         </View>
         <Text style={tw`text-gray-500 text-center font-extrabold`}>
           {user.user.display_name}
